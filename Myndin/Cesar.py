d3117e72dba9ec4a506df3b5b2e20a7f74ac22be
Tom Gouville <tom.gouville@gmail.com>
2018-05-21 16:33:19 +0200

    Correction d'un problème d'indentation

--- Myndin/Cesar.py
+++ Myndin/Cesar.py
@@ -2,39 +2,37 @@
 #Denis L.
 
 def cesar(phraseAChiffrer, decalage, alphabetChoisi): # def cesar ...
-	alphabet0 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ "
-	alphabet1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz "
-	alphabet2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 "
-	alphabet3 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789' "
-	alphabet4 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!\"#$%&'()*+,-./:;<=>?@[>]^_`{|}~ ¢£¤¥¦§¨©ª«¬®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿ\n"
-	
-	alphabet = ""
-	if alphabetChoisi == 0 :
-		alphabet = alphabet0
-	elif alphabetChoisi == 1 :
-		alphabet = alphabet1
-	elif alphabetChoisi == 2 :
-		alphabet = alphabet2
-	elif alphabetChoisi == 3 :
-		alphabet = alphabet3
-	elif alphabetChoisi == 4 :
+    alphabet0 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ "
+    alphabet1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz "
+    alphabet2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 "
+    alphabet3 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789' "
+    alphabet4 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!\"#$%&'()*+,-./:;<=>?@[>]^_`{|}~ ¢£¤¥¦§¨©ª«¬®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿ\n"
+
+    alphabet = ""
+    if alphabetChoisi == 0 :
+        alphabet = alphabet0
+    elif alphabetChoisi == 1 :
+        alphabet = alphabet1
+    elif alphabetChoisi == 2 :
+        alphabet = alphabet2
+    elif alphabetChoisi == 3 :
+        alphabet = alphabet3
+    elif alphabetChoisi == 4 :
         alphabet = alphabet4
-	else :
-		raise ValueError("Alphabet invalide")
+    else :
+        raise ValueError("Alphabet invalide")
 		
 	
 	
-	tailleAlphabet = len(alphabet)
-	sortie = ""
+    tailleAlphabet = len(alphabet)
+    sortie = ""
+
+    for lettre in phraseAChiffrer:
+        position = alphabet.find(lettre)
+        if position == -1 :
+            raise ValueError("Caractère invalide")
+        positionD = (position + decalage) % tailleAlphabet
+        lettreD = alphabet[positionD]
+        sortie = sortie + lettreD
 	
-	for lettre in phraseAChiffrer:
-		position = alphabet.find(lettre)
-		if position == -1 :
-			raise ValueError("Caractère invalide")
-		positionD = (position + decalage) % tailleAlphabet
-		lettreD = alphabet[positionD]
-		sortie = sortie + lettreD
-	
-	
-	return sortie
-	
+    return sortie
